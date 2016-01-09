charettes/django-discover-jenkins

commit 18796e1d7880a40b72cb2f616e4b8c9cba10ccf2

Removed unused support code for Python 2.6.

discover_jenkins/results.py
@@ -4,12 +4,7 @@ from datetime import datetime
 from itertools import groupby
 from xml.sax.saxutils import XMLGenerator
 from xml.sax.xmlreader import AttributesImpl
-from discover_jenkins.utils import total_seconds
-
-try:
-    from unittest import TextTestResult
-except ImportError:
-    from django.utils.unittest import TextTestResult
+from unittest import TextTestResult
 
 try:
     from django.utils.encoding import smart_text
@@ -191,8 +186,9 @@ class XMLTestResult(TextTestResult):
                     document.startElement('testcase', AttributesImpl({
                         'classname': suite_name,
                         'name': self.test_method_name(test_info.test_method),
-                        'time': '%3f' % total_seconds(
-                                    test_info.end_time - test_info.start_time)
+                        'time': '%3f' % (
+                            test_info.end_time - test_info.start_time
+                        ).total_seconds
                     }))
 
                     if test_info.result == TestInfo.RESULT.ERROR:

discover_jenkins/runner.py
@@ -1,19 +1,11 @@
 import inspect
 
+import unittest
 from optparse import make_option
+from importlib import import_module
 
 import django
 from django.core.exceptions import ImproperlyConfigured
-
-try:
-    import unittest
-except ImportError:
-    from django.utils import unittest
-
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
 
 try:
     # Django 1.6

discover_jenkins/tasks/run_pep8.py
@@ -5,7 +5,7 @@ import pep8
 from optparse import make_option
 import django
 from django.conf import settings
-from discover_jenkins.utils import check_output, get_app_locations
+from discover_jenkins.utils import get_app_locations
 
 
 class Pep8Task(object):

discover_jenkins/tasks/run_sloccount.py
@@ -2,8 +2,9 @@
 import os
 import sys
 from optparse import make_option
+import subprocess
 import django
-from discover_jenkins.utils import check_output, get_app_locations
+from discover_jenkins.utils import get_app_locations
 
 
 class SlocCountTask(object):
@@ -51,7 +52,7 @@ class SlocCountTask(object):
     def teardown_test_environment(self, **kwargs):
         locations = get_app_locations()
 
-        report_output = check_output(
+        report_output = subprocess.check_output(
             ['sloccount', "--duplicates", "--wide", "--details"] + locations
         )
         report_output = report_output.decode('utf-8')

discover_jenkins/utils.py
@@ -1,10 +1,6 @@
 import os.path
 import subprocess
-
-try:
-    from importlib import import_module
-except ImportError:
-    from django.utils.importlib import import_module
+from importlib import import_module
 
 from discover_jenkins.settings import PROJECT_APPS
 
@@ -17,28 +13,6 @@ class CalledProcessError(subprocess.CalledProcessError):
     def __str__(self):
         return ("Command '%s' returned non-zero exit status %d\nOutput:\n%s"
                 % (self.cmd, self.returncode, self.output))
-
-
-def check_output(*popenargs, **kwargs):
-    """
-    Backport from Python2.7
-    """
-    if getattr(subprocess, 'check_output', None) is None:
-        if 'stdout' in kwargs or 'stderr' in kwargs:
-            raise ValueError('stdout or stderr argument not allowed, '
-                             'it will be overridden.')
-        process = subprocess.Popen(stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE,
-                                   *popenargs, **kwargs)
-        output, err = process.communicate()
-        retcode = process.poll()
-        if retcode:
-            cmd = kwargs.get("args")
-            if cmd is None:
-                cmd = popenargs[0]
-            raise CalledProcessError(retcode, cmd, output=output + '\n' + err)
-        return output
-    return subprocess.check_output(*popenargs, **kwargs)
 
 
 def find_first_existing_executable(exe_list):
@@ -56,15 +30,6 @@ def find_first_existing_executable(exe_list):
             pass
         else:
             return filepath
-
-
-def total_seconds(delta):
-    """
-    Backport timedelta.total_seconds() from Python 2.7
-    """
-    if getattr(delta, 'total_seconds', None) is None:
-        return delta.days * 86400.0 + delta.seconds + delta.microseconds * 1e-6
-    return delta.total_seconds()
 
 
 def get_app_locations():

tests/tests/test_utils.py
@@ -1,5 +1,4 @@
 import os
-from datetime import timedelta
 
 from django.test import TestCase
 
@@ -8,13 +7,6 @@ import test_project
 
 
 class TestUtils(TestCase):
-
-    def test_total_seconds(self):
-        """
-        The total_seconds util should show that 5 minutes is 300 seconds.
-        """
-        delta = timedelta(minutes=5)
-        self.assertEqual(discover_jenkins.utils.total_seconds(delta), 300)
 
     def test_app_locations(self):
         """

commit 659580b3b3454412e7e4cd8107ac178347be0926

Fixed a typo introduced by 18796e1d7880a40b72cb2f616e4b8c9cba10ccf2.

discover_jenkins/results.py
@@ -185,7 +185,7 @@ class XMLTestResult(TextTestResult):
                         'name': self.test_method_name(test_info.test_method),
                         'time': '%3f' % (
                             test_info.end_time - test_info.start_time
-                        ).total_seconds
+                        ).total_seconds()
                     }))
 
                     if test_info.result == TestInfo.RESULT.ERROR: